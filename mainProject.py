a447fdbb2030426cc199007fe2081ab1c6e62bb7
Chike Ezeh <ezeh.chike@gmail.com>
2017-10-01 15:37:35 -0500

    add JSON end points

--- mainProject.py
+++ mainProject.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
+from flask import request, redirect, url_for, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, Base, MenuItem
@@ -12,30 +13,30 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# Fake Restaurants
-# restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-#
-# restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {
-#     'name': 'Blue Burgers', 'id': '2'}, {'name': 'Taco Hut', 'id': '3'}]
-#
-#
-# # Fake Menu Items
-# items = [{'name': 'Cheese Pizza', 'description': 'made with fresh cheese',
-#           'price': '$5.99', 'course': 'Entree', 'id': '1'},
-#        {'name': 'Chocolate Cake', 'description': 'made with Dutch Chocolate',
-#           'price': '$3.99', 'course': 'Dessert', 'id': '2'},
-#          {'name': 'Caesar Salad',
-#           'description': 'with fresh organic vegetables',
-#           'price': '$5.99', 'course': 'Entree', 'id': '3'},
-#          {'name': 'Iced Tea', 'description': 'with lemon',
-#           'price': '$.99', 'course': 'Beverage', 'id': '4'},
-#          {'name': 'Spinach Dip',
-#           'description': 'creamy dip with fresh spinach',
-#           'price': '$1.99', 'course': 'Appetizer', 'id': '5'}]
-# item = {'name': 'Cheese Pizza', 'description': 'made with fresh cheese',
-#         'price': '$5.99', 'course': 'Entree'}
+
+@app.route('/restaurants/<int:restaurant_id>/JSON')
+# json for menus in a restaurant
+def restaurantMenuJSON(restaurant_id):
+    items = session.query(
+        MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
 
 
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/JSON')
+# json for items in a restaurant menu
+def menuItemJSON(restaurant_id, menu_id):
+    Menu_Item = session.query(MenuItem).filter_by(id=menu_id).one()
+    return jsonify(Menu_Item=Menu_Item.serialize)
+
+
+@app.route('/restaurants/JSON')
+# json for restaurants
+def restaurantsJSON():
+    restaurants = session.query(Restaurant).all()
+    return jsonify(restaurants=[r.serialize for r in restaurants])
+
+
+@app.route('/')
 @app.route('/restaurants/')
 def allRestaurants():
     restaurants = session.query(Restaurant).all()
