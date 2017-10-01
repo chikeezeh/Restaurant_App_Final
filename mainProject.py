20626fb7e530293bafadf6202313cfbf1f024ffc
Chike Ezeh <ezeh.chike@gmail.com>
2017-09-30 22:19:25 -0500

    add show restaurant menu functionality

--- mainProject.py
+++ mainProject.py
@@ -83,6 +83,8 @@ def deleteRestaurant(restaurant_id):
 
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     return render_template('menu.html', restaurant=restaurant, items=items)
 
 
