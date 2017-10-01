759163fbb344a4f674d1cffd55a975fbcf6e13c4
Chike Ezeh <ezeh.chike@gmail.com>
2017-09-30 20:06:47 -0500

    add CRUD functionality to show all restaurants and add a new one

--- mainProject.py
+++ mainProject.py
@@ -1,38 +1,56 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
+
 app = Flask(__name__)
+# The following codes will connect to the database
+# and query it for data, so that it can be presented on the
+# HTML page
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 # Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {
-    'name': 'Blue Burgers', 'id': '2'}, {'name': 'Taco Hut', 'id': '3'}]
-
-
-# Fake Menu Items
-items = [{'name': 'Cheese Pizza', 'description': 'made with fresh cheese',
-          'price': '$5.99', 'course': 'Entree', 'id': '1'},
-         {'name': 'Chocolate Cake', 'description': 'made with Dutch Chocolate',
-          'price': '$3.99', 'course': 'Dessert', 'id': '2'},
-         {'name': 'Caesar Salad',
-          'description': 'with fresh organic vegetables',
-          'price': '$5.99', 'course': 'Entree', 'id': '3'},
-         {'name': 'Iced Tea', 'description': 'with lemon',
-          'price': '$.99', 'course': 'Beverage', 'id': '4'},
-         {'name': 'Spinach Dip',
-          'description': 'creamy dip with fresh spinach',
-          'price': '$1.99', 'course': 'Appetizer', 'id': '5'}]
-item = {'name': 'Cheese Pizza', 'description': 'made with fresh cheese',
-        'price': '$5.99', 'course': 'Entree'}
+# restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+#
+# restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {
+#     'name': 'Blue Burgers', 'id': '2'}, {'name': 'Taco Hut', 'id': '3'}]
+#
+#
+# # Fake Menu Items
+# items = [{'name': 'Cheese Pizza', 'description': 'made with fresh cheese',
+#           'price': '$5.99', 'course': 'Entree', 'id': '1'},
+#        {'name': 'Chocolate Cake', 'description': 'made with Dutch Chocolate',
+#           'price': '$3.99', 'course': 'Dessert', 'id': '2'},
+#          {'name': 'Caesar Salad',
+#           'description': 'with fresh organic vegetables',
+#           'price': '$5.99', 'course': 'Entree', 'id': '3'},
+#          {'name': 'Iced Tea', 'description': 'with lemon',
+#           'price': '$.99', 'course': 'Beverage', 'id': '4'},
+#          {'name': 'Spinach Dip',
+#           'description': 'creamy dip with fresh spinach',
+#           'price': '$1.99', 'course': 'Appetizer', 'id': '5'}]
+# item = {'name': 'Cheese Pizza', 'description': 'made with fresh cheese',
+#         'price': '$5.99', 'course': 'Entree'}
 
 
 @app.route('/restaurants/')
 def allRestaurants():
+    restaurants = session.query(Restaurant)
     return render_template('restaurants.html', restaurants=restaurants)
 
 
 @app.route('/restaurants/new', methods=['GET', 'POST'])
 def newRestaurant():
-    return render_template('newRestaurant.html')
+    if request.method == 'POST':
+        newRestaurant = Restaurant(name=request.form['name'])
+        session.add(newRestaurant)
+        session.commit()
+        return redirect(url_for('allRestaurants'))
+    else:
+        return render_template('newRestaurant.html')
 
 
 @app.route('/restaurants/<int:restaurant_id>/edit',  methods=['GET', 'POST'])
